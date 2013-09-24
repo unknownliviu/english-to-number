99f8ab9919ee102c8c4b532d256f356964896efa
liviu <unknownliviu@gmail.com>
2013-09-24 13:08:58 +0300

    initial commit

---
+++ numberfile.rb
@@ -0,0 +1,43 @@
+load 'numberfile_helper.rb'
+
+super_hash = initialise_hash
+
+input = gets.chomp.downcase.gsub(/[^a-z\s]/, '').split ' '
+
+numbers = []
+
+input.each { |word| numbers << super_hash[word] unless super_hash[word].nil? }
+
+i=sum=stack=0
+
+while i<numbers.length
+  
+  n = numbers[i]
+  ni= numbers[i-1]
+
+  if i==0 || stack==0
+    i+=1
+    stack = n
+    next
+  end
+
+  if n>=1000
+    sum+=stack*n
+    stack=0
+  else
+    if ni<n
+      stack*=n
+    else
+      stack+=n
+    end
+  end
+  if ni>=1000 && n<1000
+    sum+=stack
+    stack=n
+  end
+
+  i+=1
+end 
+sum+=stack
+
+puts sum
---
+++ numberfile_helper.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+def initialise_hash
+digits=%w[zero one two three four five six seven eight nine]
+digits_hash = Hash[digits.map.with_index.to_a]
+
+teens=%w[ten eleven twelve thirtheen fourteen fifteen sixteen seventeen eighteen nineteen]
+
+teens_hash = Hash[teens.each_with_index.map {|x, i| [x, i+10]}]
+
+tens=%w[twenty thirty forty fifty sixty seventy eighty ninety hundred]
+
+tens_hash = Hash[tens.each_with_index.map {|x, i| [x, (i+2)*10]}]
+
+multipliers = %w[thousand million billion trillion quadrillion quintillion 
+  sextillion septillion octillion nonillion decillion undecillion 
+  duodecillion tredecillion quattuordecillion quindecillion]
+
+multipliers_hash = Hash[multipliers.each_with_index.map{ |x, i| [x, (1000**(i+1)) ]}]
+
+super_hash = digits_hash.merge(teens_hash).merge(tens_hash).merge(multipliers_hash)
+end
